6707a5442b8bb8dc54457997b0f7350b9640f631
Dasmeliox <ismail.isabekov@mail.ru>
2022-06-16 20:22:46 +0600

    add POST category

--- apps/category/urls.py
+++ apps/category/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, CategoryCreateView
 
 urlpatterns = [
     path('categories/', CategoryListView.as_view()),
+    path('create/', CategoryCreateView.as_view())
 ]
--- apps/product/serializers.py
+++ apps/product/serializers.py
@@ -1,8 +1,8 @@
 from rest_framework import serializers
 from .models import ProductImage, Product
-# from apps.review.serializers import ReviewSerializer
-# from apps.comment.models import Comment
-# from apps.comment.serializers import CommentSerializer
+from apps.review.serializers import ReviewSerializer
+from apps.comment.models import Comment
+from apps.comment.serializers import CommentSerializer
 
 
 class ProductImageSerializer(serializers.ModelSerializer):
@@ -10,20 +10,20 @@ class ProductImageSerializer(serializers.ModelSerializer):
         model = ProductImage
         fields = ('image',)
 
-    # def _get_image_url(self, obj):
-    #     if obj.image:
-    #         url = obj.image.url
-    #         requests = self.context.get("request")
-    #         if requests is not None:
-    #             url = requests.build_absolute_uri(url)
-    #     else:
-    #         url = ""
-    #     return url
+    def _get_image_url(self, obj):
+        if obj.image:
+            url = obj.image.url
+            requests = self.context.get("request")
+            if requests is not None:
+                url = requests.build_absolute_uri(url)
+        else:
+            url = ""
+        return url
 
-    # def to_representation(self, instance):
-    #     rep = super().to_representation(instance)
-    #     rep['image'] = self._get_image_url(instance)
-    #     return rep
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['image'] = self._get_image_url(instance)
+        return rep
 
 
 class ProductSerializer(serializers.ModelSerializer):
@@ -31,14 +31,14 @@ class ProductSerializer(serializers.ModelSerializer):
         model = Product
         fields = "__all__"
 
-    # def to_representation(self, instance):
-    #     rep = super().to_representation(instance)
-    #     rep['images'] = ProductImageSerializer(ProductImage.objects.filter(product=instance.id), many=True).data
-    #     rep['reviews'] = ReviewSerializer(instance.review.filter(product=instance.id), many=True).data
-    #     total_rating = [i.rating for i in instance.review.all()]
-    #     if len(total_rating) != 0:
-    #         rep['total_rating'] = sum(total_rating) / len(total_rating)
-    #     else:
-    #         rep['total_rating'] = ""
-    #     rep['comments'] = CommentSerializer(Comment.objects.filter(product_id=instance), many=True).data
-    #     return rep
+    def to_representation(self, instance):
+        rep = super().to_representation(instance)
+        rep['images'] = ProductImageSerializer(ProductImage.objects.filter(product=instance.id), many=True).data
+        rep['reviews'] = ReviewSerializer(instance.review.filter(product=instance.id), many=True).data
+        total_rating = [i.rating for i in instance.review.all()]
+        if len(total_rating) != 0:
+            rep['total_rating'] = sum(total_rating) / len(total_rating)
+        else:
+            rep['total_rating'] = ""
+        rep['comments'] = CommentSerializer(Comment.objects.filter(product_id=instance), many=True).data
+        return rep
